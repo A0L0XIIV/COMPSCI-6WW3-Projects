9f36705ce81766a0006320ce9ddd8179a74388fd
A0L0XIIV <brnky_95@hotmail.com>
2019-10-13 20:09:41 -0400

    OpenStreetMap first commit

--- Projects/Project 2/JS/main.js
+++ Projects/Project 2/JS/main.js
@@ -1,20 +1,32 @@
-function validateEmail(phone) {
+function validateEmail(email) {
+  // New pattern for emails
+  var emailPattern = new RegExp("[^@]+@[^.]+..+");
+  // Return its comparision result
+  return emailPattern.test(email);
+}
+
+function validatePassword(password) {
+  // New pattern for passwords
+  var passwordPattern = new RegExp(
+    "^(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[-_=+!@#$%^&*()]).{8,15}$"
+  );
+  // Return its comparision result
+  return passwordPattern.test(password);
+}
+
+function validateName(name) {
+  // New pattern for names
+  var namePattern = new RegExp("");
+  // Return its comparision result
+  return namePattern.test(name);
+}
+
+function validateParkName(parkName) {}
+
+function validatePhone(phoneNumber) {
   /*9055259140
 905 525 9140
 (905) 525 9140
 (905) 525 -9140
 +1 (905) 525 -9140*/
 }
-
-function validatePassword(password) {
-  var passwordPattern = new RegExp(
-    "^(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[-_=+!@#$%^&*()]).{8,15}$"
-  );
-  return passwordPattern.test(password);
-}
-
-function validateName(name) {
-  var namePattern = new RegExp("");
-}
-
-function validateParkName(parkName) {}
