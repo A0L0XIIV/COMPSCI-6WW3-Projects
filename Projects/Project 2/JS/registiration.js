91cdad1648c4f085a923f522e9d2eade6fa364f8
A0L0XIIV <brnky_95@hotmail.com>
2019-10-21 20:54:07 -0400

    Register page JS validation fixed

--- Projects/Project 2/JS/registiration.js
+++ Projects/Project 2/JS/registiration.js
@@ -2,51 +2,55 @@
 window.onload = init;
 
 // Global variables
-const form;
-const username;
-const email;
-const password;
-const city;
-const phone;
-const termsAndPrivacy;
-const usernameError;
-const emailError;
-const passwordError;
-const cityError;
-const phoneError;
-const termsAndPrivacyErrror;
+var form;
+var username;
+var email;
+var password;
+var city;
+var phone;
+var termsAndPrivacy;
+var usernameError;
+var emailError;
+var passwordError;
+var cityError;
+var phoneError;
+var termsAndPrivacyErrror;
 
-// Declare global variables in init function
+// Assign global variables in init function
 function init() {
-    // Get inputs by id
-    form = $("#registerForm");
-    username = $("#username");
-    email = $("#userEmail");
-    password = $("#userPassword");
-    city = $("#userCity");
-    phone = $("#userPhone");
-    termsAndPrivacy = $("#termsAndPrivacy");
-    // Get error panels by id
-    usernameError = $("#usernameError");
-    emailError = $("#emailError");
-    passwordError = $("#passwordError");
-    cityError = $("#cityError");
-    phoneError = $("#phoneError");
-    termsAndPrivacyErrror = $("#termsAndPrivacyErrror");
+  // Get form inputs by name
+  form = document.forms["registerForm"];
+  username = document.forms["registerForm"]["username"];
+  email = document.forms["registerForm"]["userEmail"];
+  password = document.forms["registerForm"]["userPassword"];
+  city = document.forms["registerForm"]["userCity"];
+  phone = document.forms["registerForm"]["userPhone"];
+  termsAndPrivacy = document.forms["registerForm"]["termsAndPrivacy"];
+  // Get error panels by name
+  usernameError = document.forms["registerForm"]["usernameError"];
+  emailError = document.forms["registerForm"]["emailError"];
+  passwordError = document.forms["registerForm"]["passwordError"];
+  cityError = document.forms["registerForm"]["cityError"];
+  phoneError = document.forms["registerForm"]["phoneError"];
+  termsAndPrivacyErrror =
+    document.forms["registerForm"]["termsAndPrivacyErrror"];
 }
 
 // Check required inputs for empty values
 function emptyCheck() {
   if (username.value === "" || username.value == null) {
-    usernameError.text("Username cannot be empty!");
+    alert("Username cannot be empty!");
+    usernameError.innerHTML("Username cannot be empty!");
     return false;
   }
   if (email.value === "" || email.value == null) {
-    emailError.text("Email cannot be empty!");
+    alert("Email cannot be empty!");
+    emailError.innerHTML("Email cannot be empty!");
     return false;
   }
   if (password.value === "" || password.value == null) {
-    passwordError.text("Password cannot be empty!");
+    alert("Password cannot be empty!");
+    passwordError.innerHTML("Password cannot be empty!");
     return false;
   }
   return true;
@@ -55,7 +59,8 @@ function emptyCheck() {
 // Terms & Privacy check -> Should be check in order to submit
 function termsAndPrivacyCheck() {
   if (termsAndPrivacy.value === false) {
-    termsAndPrivacyErrror.text("Please read an accept the Terms&Privicy.");
+    alert("Please read an accept the Terms&Privicy.");
+    termsAndPrivacyErrror.innerHTML("Please read an accept the Terms&Privicy.");
     return false;
   } else {
     return true;
@@ -65,12 +70,15 @@ function termsAndPrivacyCheck() {
 // Email validation and error messages
 function validateEmail() {
   // New pattern for emails
-  var emailPattern = new RegExp("/^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/");
+  var emailPattern = new RegExp(
+    "^[A-Za-z0-9_]+([.-]?[A-Za-z0-9_]+)*@[A-Za-z0-9_]+([.-]?[A-Za-z0-9_]+)*(.[A-Za-z0-9_]{2,3})+$"
+  );
   // Return its comparision result and show the errors
-  if (emailPattern.test(email)) {
+  if (emailPattern.test(email.value)) {
     return true;
   } else {
-    emailError.text("Email doesn't seem right. Please check it again.");
+    alert("Email doesn't seem right. Please check it again.");
+    emailError.innerHTML("Email doesn't seem right. Please check it again.");
     return false;
   }
 }
@@ -80,39 +88,56 @@ function validatePassword() {
   // New pattern for passwords
   // lowercase, uppercase, digit, special char. and length:8-15
   var passwordPattern = new RegExp(
-    "^(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[-_=+!@#$%^&*()]).{8,15}$"
+    "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[-_=+!@#$%^&*()]).{8,15}$"
   );
   // Return its comparision result and show the errors
-  if (passwordPattern.test(password)) {
+  if (passwordPattern.test(password.value)) {
     return true;
   } else {
-    passwordError.text(
+    alert(
+      "Password doesn't fit the pattern. At least 1 lowercase, 1 uppercase, 1 number and one special character."
+    );
+    passwordError.innerHTML(
       "Password doesn't fit the pattern. At least 1 lowercase, 1 uppercase, 1 number and one special character."
     );
     return false;
   }
 }
 
-// Username and city validation and error messages
-function validateName(name, isUsername) {
+// Username validation and error messages
+function validateUsername() {
   // New pattern for names
-  var namePattern = new RegExp("^([-_!&*()']*[a-zA-Z0-9]+[-_!&*()']*)+$");
+  var usernamePattern = new RegExp("^([-_!&*()']*[a-zA-Z0-9]+[-_!&*()']*)+$");
   // Return its comparision result and show the errors
-  if (namePattern.test(name)) {
+  if (usernamePattern.test(username.value)) {
     return true;
   } else {
-    // This function called with both username and city name
-    if (isUsername) {
-      usernameError.text("Please just use letters, numbers and -_!&*()'");
-    } else {
-      cityError.text("Please just use letters, numbers and -&'");
-    }
+    alert("Username invalid! Please just use letters, numbers and -_!&*()'");
+    usernameError.innerHTML("Please just use letters, numbers and -_!&*()'");
+    return false;
+  }
+}
+
+// City name validation and error messages
+function validateCity() {
+  // Check if city is null or not
+  if (city.value === null || city.value === "") return true;
+  // New pattern for names
+  var cityPattern = new RegExp("^([a-zA-Z0-9]+[-&']*)+$");
+  // Return its comparision result and show the errors
+  if (cityPattern.test(city.value)) {
+    return true;
+  } else {
+    alert("City name invalid! Please just use letters, numbers and -&'");
+    cityError.innerHTML("Please just use letters, numbers and -&'");
     return false;
   }
 }
 
 // Phone validation and error messages
 function validatePhone() {
+  // Check if city is null or not
+  if (phone.value === null || phone.value === "") return true;
   /*9055259140
   905 525 9140
   (905) 525 9140
@@ -123,10 +148,11 @@ function validatePhone() {
     "^[+]{0,1}[0-9s]{0,4}[(]{0,1}[0-9]{1,4}[)]{0,1}[-s./0-9]*$"
   );
   // Return its comparision result and show the errors
-  if (phonePattern.test(phone)) {
+  if (phonePattern.test(phone.value)) {
     return true;
   } else {
-    phoneError.text("Phone number doesn't seem right. Please try again.");
+    alert("Phone number doesn't seem right. Please try again.");
+    phoneError.innerHTML("Phone number doesn't seem right. Please try again.");
     return false;
   }
 }
@@ -139,14 +165,14 @@ function validateForm() {
       emptyCheck() &&
       validateEmail() &&
       validatePassword() &&
-      validateName(username, true) &&
-      validateName(city, false) &&
+      validateUsername() &&
+      validateCity() &&
       validatePhone() &&
       termsAndPrivacyCheck()
     ) {
-      alert("OK");
+      alert("Form is valid!");
     } else {
-      alert("NOT OK");
+      alert("Form is NOT valid!");
     }
   }
 }
