2730cac7b39beb84623b75be20b1bf3e1de0278e
A0L0XIIV <brnky_95@hotmail.com>
2019-11-27 17:39:31 -0500

    Result page's Maps fixed

--- Projects/Project 3/JS/result_sample.js
+++ Projects/Project 3/JS/result_sample.js
@@ -1,24 +1,36 @@
 // Init function -> body calls it
 function init() {
-  // Set dynamic variables in init function
-  // Set dynamic variables in init function
-  var parkName = $(".parkName").text();
-  var latitude = $("#latitude").text();
-  var longitude = $("#longitude").text();
+  var i = 0;
+  var parkIds = [];
+  var parkNames = [];
+  var parkLatitudes = [];
+  var parkLongitudes = [];
+  // Loop over each result
+  while ($("#map" + i).length) {
+    // Get park name, latitude and longitude values
+    var parkId = $("#parkId" + i).text();
+    var parkName = $("#parkName" + i).text();
+    var latitude = $("#latitude" + i).text();
+    var longitude = $("#longitude" + i).text();
+    // Load Tabular results' maps with mapId
+    parkMap(parkName, latitude, longitude, "map" + i);
+    i++;
+    // Push values for one map result tab
+    parkIds.push(parkId);
+    parkNames.push(parkName);
+    parkLatitudes.push(latitude);
+    parkLongitudes.push(longitude);
+  }
 
-  // Load Tabular results' maps -> 4 results, 4 maps
-  parkMap(parkName, latitude, longitude, "map1");
-  parkMap("Chedoke Civic Golf Course", 43.248, -79.909, "map2");
-  parkMap("Victoria Park", 43.263, -79.884, "map3");
-  parkMap("Gage Park", 43.241, -79.83, "map4");
   // Map Results -> 1 map, 4 markers
-  parkMap(parkName, latitude, longitude, "mapResultsMap");
+  parksMap(parkIds, parkNames, parkLatitudes, parkLongitudes, "mapResultsMap");
 }
 
 // LeafletJS and OpenStreetMap function
-function parkMap(parkDescription, coordinate_x, coordinate_y, name) {
+// Tabular Results page function
+function parkMap(parkName, coordinate_x, coordinate_y, mapId) {
   // Draw map w/ coordinates
-  var map = L.map(name).setView([coordinate_x, coordinate_y], 15);
+  var map = L.map(mapId).setView([coordinate_x, coordinate_y], 15);
 
   // Set layers of the map and min/max zoom
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -36,20 +48,49 @@ function parkMap(parkDescription, coordinate_x, coordinate_y, name) {
   }
 
   // Add marker or markers on map
-  // Map Results page
-  if (name === "mapResultsMap") {
-    var link =
-      "<a href='../PagesToDevelop/individual_sample.html'>" +
-      parkDescription +
-      "</a>";
-    addMarker(map, coordinate_x, coordinate_y, link);
-    addMarker(map, 43.248, -79.909, link);
-    addMarker(map, 43.263, -79.884, link);
-    addMarker(map, 43.241, -79.83, link);
+  addMarker(map, coordinate_x, coordinate_y, parkName);
+
+  // When Tabular Results tab opened invalidateSize -> Show map
+  $("a[href='#tabularResults']").on("shown.bs.tab", function(e) {
+    map.invalidateSize();
+  });
+
+  // When Map Results tab opened invalidateSize -> Show map
+  // Without this Map Results tab's map doesn't show
+  $("a[href='#mapResults']").on("shown.bs.tab", function(e) {
+    map.invalidateSize();
+  });
+}
+
+// Map Results page function
+function parksMap(parkIds, parkNames, parkLatitudes, parkLongitudes, mapId) {
+  // Draw map w/ coordinates
+  var map = L.map(mapId).setView([parkLatitudes[0], parkLongitudes[0]], 15);
+
+  // Set layers of the map and min/max zoom
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+    maxZoom: 18,
+    minZoom: 1
+  }).addTo(map);
+
+  // On small screens, make attribution text smaller
+  if ($(window).width() <= 500) {
+    $(".leaflet-control-attribution").css({
+      fontSize: 5.5
+    });
   }
-  // Tabular Results page
-  else {
-    addMarker(map, coordinate_x, coordinate_y, parkDescription);
+
+  // Add markers on map
+  for (var i = 0; parkNames[i]; i++) {
+    var link =
+      "<a href='../PHP/individual_sample.php?get-park&id=" +
+      parkIds[i] +
+      "'>" +
+      parkNames[i] +
+      "</a>";
+    addMarker(map, parkLatitudes[i], parkLongitudes[i], link);
   }
 
   // When Tabular Results tab opened invalidateSize -> Show map
